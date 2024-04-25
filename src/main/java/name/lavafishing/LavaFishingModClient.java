b3cd20fbb86bc2588a92662d27fbd6f783f62a2b
fangyi <2370145097@qq.com>
2024-04-25 11:21:14 +0800

    1.20.4
    Signed-off-by: fangyi <2370145097@qq.com>

--- src/main/java/name/lavafishing/LavaFishingModClient.java
+++ src/main/java/name/lavafishing/LavaFishingModClient.java
@@ -1,13 +1,40 @@
 package name.lavafishing;
 
+import name.lavafishing.client.FishingBobberEntityRendererClient;
+import name.lavafishing.registry.LavafishingItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.FishingBobberEntityRenderer;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FishingRodItem;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
-public class LavaFishingModClient extends LavaFishing implements ClientModInitializer {
+public class LavaFishingModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient(){
+        EntityRendererRegistry.register(EntityType.FISHING_BOBBER, FishingBobberEntityRendererClient::new);
+        registerFishingRodPredicates(LavafishingItems.OBSIDIAN_FISHING_ROD);
+    }
 
+    public void registerFishingRodPredicates(Item item) {
+        ModelPredicateProviderRegistry.register(item, new Identifier("cast"), (itemStack, clientWorld, livingEntity, i) -> {
+            if (livingEntity == null) {
+                return 0.0F;
+            } else {
+                boolean bl = livingEntity.getMainHandStack() == itemStack;
+                boolean bl2 = livingEntity.getOffHandStack() == itemStack;
+                if (livingEntity.getMainHandStack().getItem() instanceof FishingRodItem) {
+                    bl2 = false;
+                }
+                return (bl || bl2) && livingEntity instanceof PlayerEntity && ((PlayerEntity)livingEntity).fishHook != null ? 1.0F : 0.0F;
+            }
+        });
     }
 }
--- src/main/java/name/lavafishing/item/ItemObsidianFishingRod.java
+++ src/main/java/name/lavafishing/item/ItemObsidianFishingRod.java
@@ -23,71 +23,33 @@ public class ItemObsidianFishingRod extends FishingRodItem {
         super(settings);
     }
 
-//    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-//        ItemStack itemStack = user.getStackInHand(hand);
-//        int i;
-//        if (user.fishHook != null) {
-//            if (!world.isClient) {
-//                i = user.fishHook.use(itemStack);
-//                itemStack.damage(i, user, (p) -> {
-//                    p.sendToolBreakStatus(hand);
-//                });
-//            }
-//
-//            world.playSound((PlayerEntity)null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.NEUTRAL, 1.0F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
-//            user.emitGameEvent(GameEvent.ITEM_INTERACT_FINISH);
-//        } else {
-//            world.playSound((PlayerEntity)null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
-//            if (!world.isClient) {
-//                i = EnchantmentHelper.getLure(itemStack);
-//                int j = EnchantmentHelper.getLuckOfTheSea(itemStack);
-//                world.spawnEntity(new FishingBobberEntity(user, world, j, i));
-//            }
-//
-//            user.incrementStat(Stats.USED.getOrCreateStat(this));
-//            user.emitGameEvent(GameEvent.ITEM_INTERACT_START);
-//        }
-//
-//        return TypedActionResult.success(itemStack, world.isClient());
-//    }
-//
-//
-
-    @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack heldStack = user.getStackInHand(hand);
-
-
-        if(user.fishHook != null) {
-            // Retrieve fishing bobber and damage Fishing Rod
-            if(!world.isClient) {
-                int damage = user.fishHook.use(heldStack);
-                heldStack.damage(damage, user, player -> player.sendToolBreakStatus(hand));
+        ItemStack itemStack = user.getStackInHand(hand);
+        int i;
+        if (user.fishHook != null) {
+            if (!world.isClient) {
+                i = user.fishHook.use(itemStack);
+                itemStack.damage(i, user, (p) -> {
+                    p.sendToolBreakStatus(hand);
+                });
             }
 
-            world.playSound(null, user.getX(), user.getY(), user.getZ(),SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.NEUTRAL, 1.0F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
+            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.NEUTRAL, 1.0F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
+            user.emitGameEvent(GameEvent.ITEM_INTERACT_FINISH);
         } else {
-            world.playSound(null, user.getX(), user.getY(), user.getZ(),SoundEvents.ENTITY_FISHING_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
-
-            // Summon new fishing bobber
-            if(!world.isClient) {
-                int bonusExperience = 0;
-
-                // Calculate lure and luck
-                int lure = Math.min((EnchantmentHelper.getLure(heldStack)),5);
-                int lots = EnchantmentHelper.getLuckOfTheSea(heldStack);
-
-                // Summon bobber with stats
-                FishingBobberEntity bobber = new FishingBobberEntity(user, world, lots, lure);
-                world.spawnEntity(bobber);
+            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
+            if (!world.isClient) {
+                i = EnchantmentHelper.getLure(itemStack);
+                int j = EnchantmentHelper.getLuckOfTheSea(itemStack);
+                world.spawnEntity(new FishingBobberEntity(user, world, j, i));
             }
 
             user.incrementStat(Stats.USED.getOrCreateStat(this));
+            user.emitGameEvent(GameEvent.ITEM_INTERACT_START);
         }
 
-        return TypedActionResult.success(heldStack, world.isClient());
+        return TypedActionResult.success(itemStack, world.isClient());
     }
-
 
     @Override
     public int getEnchantability() {
